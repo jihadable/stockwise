7f7815f67074c137d04088988770ebf936d2aeea
jihadable <umarjihad141004@gmail.com>
2026-02-22 21:08:40 +0700

    create verify email page

--- src/pages/VerifyEmail.tsx
+++ src/pages/VerifyEmail.tsx
@@ -1,6 +1,8 @@
 import { IconArrowLeft, IconCheck, IconX } from "@tabler/icons-react"
+import axios from "axios"
 import { useEffect, useState } from "react"
 import { Link, useParams } from "react-router-dom"
+import logo from "../assets/icon.png"
 import "../style/VerifyEmail.css"
 import NotFound from "./NotFound"
 
@@ -15,7 +17,12 @@ export default function VerifyEmail(){
     useEffect(() => {
         const handleVerifyEmail = async() => {
             try {
-                
+                const APIEndpoint = import.meta.env.VITE_API_ENDPOINT
+                await axios.post(`${APIEndpoint}/api/email-verifications/verify-email`, {
+                    token
+                })
+
+                setStatus("success")
             } catch(error){
                 setStatus("error")
                 console.log(error)
@@ -35,7 +42,10 @@ export default function VerifyEmail(){
     if (status == "error"){
         return (
             <section className="verify-error">
-                <div className="logo-title"></div>
+                <div className="logo-title">
+                    <img src={logo} alt="Logo" />
+                    <h1>Stockwise</h1>
+                </div>
                 <div className="message"><IconX /> <p>Verification failed</p></div>
                 <p className="text">This verification link is invalid or has expired.</p>
                 <Link to={"/"} className="link">
@@ -48,7 +58,10 @@ export default function VerifyEmail(){
     if (status == "success"){
         return (
             <section className="verify-success">
-                <div className="logo-title"></div>
+                <div className="logo-title">
+                    <img src={logo} alt="Logo" />
+                    <h1>Stockwise</h1>
+                </div>
                 <div className="message"><IconCheck /> <p>Email verified</p></div>
                 <p className="text">Your email address has been successfully verified.</p>
                 <Link to={"/dashboard"} className="link">
