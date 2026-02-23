6b4ab50c542eeff3f58541774a02e3935dd9e377
jihadable <umarjihad141004@gmail.com>
2026-02-23 13:35:56 +0700

    remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'react-toastify/dist/ReactToastify.css'
 import Router from './Router'
 import './style/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-<React.StrictMode>
-    <Router />
-</React.StrictMode>)
+ReactDOM.createRoot(document.getElementById('root')!).render(<Router />)
