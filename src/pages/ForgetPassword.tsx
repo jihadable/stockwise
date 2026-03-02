cea57a9e6865c1858b140af209cad358d2168591
jihadable <umarjihad141004@gmail.com>
2026-03-02 14:22:09 +0700

    create reset password page

--- src/pages/ForgetPassword.tsx
+++ src/pages/ForgetPassword.tsx
@@ -1,12 +1,16 @@
 import { IconMail } from "@tabler/icons-react";
 import axios from "axios";
 import { FormEvent, useContext, useState } from "react";
+import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import Loader from "../components/Loader";
 import { LoaderContext } from "../contexts/LoaderContext";
 import "../style/ForgetPassword.css";
 
 export default function ForgetPassword(){
+
+    document.title = "StockWise | Forget Password"
+
     const [email, setEmail] = useState<string>("")
     const [isLoading, setIsLoading] = useState<boolean>(false)
     const { setLoadingElementWidth, setLoadingElementHeight } = useContext(LoaderContext)
@@ -42,12 +46,13 @@ export default function ForgetPassword(){
                 <p>Forgotten your password? Enter your email address below, and we'll send you an email allowing you to reset it.</p>
                 <div className="email">
                     <IconMail stroke={1.5} />
-                    <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input type="text" placeholder="Email" required value={email} onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 {isLoading ?
                 <Loader /> :
                 <button type="submit">Submit</button>}
             </form>
+            <Link to={"/login"}>Login</Link>
         </section>
     )
 }
--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -1,7 +1,78 @@
+import { IconLock } from "@tabler/icons-react"
+import axios from "axios"
+import { FormEvent, useContext, useState } from "react"
+import { Link, useParams } from "react-router-dom"
+import { toast } from "react-toastify"
+import Loader from "../components/Loader"
+import { LoaderContext } from "../contexts/LoaderContext"
+import "../style/ResetPassword.css"
+import NotFound from "./NotFound"
+
 export default function ResetPassword(){
+    const { token } = useParams<string>()
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const { setLoadingElementWidth, setLoadingElementHeight } = useContext(LoaderContext)
+    const [newPassword, setNewPassword] = useState<string>("")
+    const [newPasswordConfirmation, setNewPasswordConfirmation] = useState<string>("")
+
+    const handleResetPassword = async(event: FormEvent) => {
+        try {
+            event.preventDefault()
+
+            if (newPassword != newPasswordConfirmation){
+                toast.warn("New password confirmation does not match")
+
+                return
+            }
+
+            const target = event.currentTarget as HTMLFormElement
+            setIsLoading(true)
+            setLoadingElementWidth(target.querySelector('button[type="submit"]')?.clientWidth)
+            setLoadingElementHeight(target.querySelector('button[type="submit"]')?.clientHeight)
+
+            const requestBody = {
+                token,
+                new_password: newPassword
+            }
+
+            const APIEndpoint = import.meta.env.VITE_API_ENDPOINT
+            await axios.post(`${APIEndpoint}/api/password-resets/reset-password`, requestBody)
+
+            setNewPassword("")
+            setNewPasswordConfirmation("")
+
+            toast.success("Password reset successfully")
+            setIsLoading(false)
+        } catch(error){
+            toast.error("Fail to reset password")
+            setIsLoading(false)
+            console.log(error)
+        }
+    }
+
+    if (!token){
+        return <NotFound />
+    }
+
+    document.title = "StockWise | Reset Password"
+    
     return (
-        <section>
-            
+        <section className="reset-password" onSubmit={handleResetPassword}>
+            <form>
+                <h2>Reset Password</h2>
+                <div className="new-password">
+                    <IconLock stroke={1.5} />
+                    <input type="password" placeholder="New password" required value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
+                </div>
+                <div className="confirm-new-password">
+                    <IconLock stroke={1.5} />
+                    <input type="password" placeholder="Confirm new password" required value={newPasswordConfirmation} onChange={(e) => setNewPasswordConfirmation(e.target.value)} />
+                </div>
+                {isLoading ?
+                <Loader /> :
+                <button type="submit">Submit</button>}
+            </form>
+            <Link to={"/login"}>Login</Link>
         </section>
     )
 }
